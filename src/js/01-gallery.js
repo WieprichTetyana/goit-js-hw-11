6a78f852b80e4cb979e979b8dde053a012984afb
Maria Yeromina <mariayeromina@Marias-MacBook-Air.local>
2023-11-08 15:04:11 +0100

    Summary hw8

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from "simplelightbox";
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+
+import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector("ul.gallery");
+
+function createMarkUp({ preview, original, description }) {
+    return `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}" data-caption="${description}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+};
+const galleryCartArray = galleryItems.map(item => createMarkUp(item));
+console.log(galleryCartArray);
+
+galleryList.innerHTML = galleryCartArray.join("");
+
+new SimpleLightbox(".gallery a", { captions: true, captionsData: "alt", captionsDelay: 250 });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle  from 'lodash.throttle';
 
+const contactFormEl = document.querySelector('.js-feedback-form');
+
+const userData = {};
+
+const fillFeedbackFormField = () => {
+  const userDataFromLS = localStorage.getItem('contactFormInfo');
+
+if (userDataFromLS) {
+  console.log(userDataFromLS);
+  const userDataParsed = JSON.parse(userDataFromLS);
+
+  for (const key in userDataParsed) {
+    if (userDataParsed.hasOwnProperty(key)) {
+      contactFormEl.elements[key].value = userDataParsed[key];
+    }
+  }
+}
+};
+
+fillFeedbackFormField();
+
+const feedbackFormStateThrottle = throttle(({ target: feedbackFormField }) => {
+  const feedbackFormFieldValue = feedbackFormField.value;
+  const feedbackFormFieldName = feedbackFormField.name;
+
+  userData[feedbackFormFieldName] = feedbackFormFieldValue;
+
+  localStorage.setItem('contactFormInfo', JSON.stringify(userData));
+}, 500);
+
+const feedbackFormSubmit = event => {
+  event.preventDefault();
+
+  contactFormEl.reset();
+  localStorage.removeItem('contactFormInfo');
+};
+
+contactFormEl.addEventListener('change', feedbackFormStateThrottle);
+contactFormEl.addEventListener('submit', feedbackFormSubmit);
