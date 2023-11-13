fa1ef44c7c3d618a7592339b922196f2917585bd
Maria Yeromina <mariayeromina@Marias-MacBook-Air.local>
2023-11-13 13:12:37 +0100

    summary

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,37 @@
+ import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      const promise = { position, delay };
+      if (shouldResolve) {
+        resolve(promise);
+      } else {
+        reject(promise);
+      }
+      }, delay);
+  });
   }
-}
+const formElem = document.querySelector(".form");
+
+formElem.addEventListener("submit", onFormElemSub);
+ function onFormElemSub(elem) {
+   elem.preventDefault();
+
+   const step = elem.currentTarget.elements.step.value;
+   const delay = elem.currentTarget.elements.delay.value;
+   const amount = elem.currentTarget.elements.amount.value;
+ 
+   for (let i = 0; i < amount; i++) {
+     const myDelay = delay + i * step;
+
+     createPromise(i, myDelay)
+       .then(({ position, delay }) => {
+         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
+       })
+       .catch(({ position, delay }) => {
+         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+       });
+   }
+ }
