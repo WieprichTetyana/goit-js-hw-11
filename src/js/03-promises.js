6362eb8e10d39281a9ad94c2bd6bac13f7e2ebda
Maria Yeromina <mariayeromina@Marias-MacBook-Air.local>
2023-11-13 17:04:52 +0100

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
- import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.currentTarget);
+  const delay = Number(formData.get('delay'));
+  const step = Number(formData.get('step'));
+  const amount = Number(formData.get('amount'));
+
+  if (isNaN(delay) || isNaN(step) || isNaN(amount)) {
+    Notify.failure('Please enter valid numbers for delay, step, and amount.');
+    return;
+  }
+
+  let delayValue = delay;
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delayValue)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+
+    delayValue += step;
+  }
+}
 
 function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+
   return new Promise((resolve, reject) => {
-    const shouldResolve = Math.random() > 0.3;
     setTimeout(() => {
       const promise = { position, delay };
       if (shouldResolve) {
@@ -10,28 +43,6 @@ function createPromise(position, delay) {
       } else {
         reject(promise);
       }
-      }, delay);
+    }, delay);
   });
-  }
-const formElem = document.querySelector(".form");
-
-formElem.addEventListener("submit", onFormElemSub);
- function onFormElemSub(elem) {
-   elem.preventDefault();
-
-   const step = elem.currentTarget.elements.step.value;
-   const delay = elem.currentTarget.elements.delay.value;
-   const amount = elem.currentTarget.elements.amount.value;
- 
-   for (let i = 0; i < amount; i++) {
-     const myDelay = delay + i * step;
-
-     createPromise(i, myDelay)
-       .then(({ position, delay }) => {
-         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`)
-       })
-       .catch(({ position, delay }) => {
-         Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
-       });
-   }
- }
+}
