87e9519e70d4f47dcfa8f41afb0b4ed57076abbb
WieprichTetyana <143502862+WieprichTetyana@users.noreply.github.com>
2023-11-17 18:52:04 +0000

    Deploying to gh-pages from @ WieprichTetyana/goit-js-hw-11@2822ee690c2c91e75d2b757d845011f164fa451a 🚀

--- 01-color-switcher.9cad8ed9.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.getElementById("startBtn"),n=document.getElementById("stopBtn"),e=null;t.addEventListener("click",(function(){t.disabled=!0,n.disabled=!1,e=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3)})),n.addEventListener("click",(function(){t.disabled=!1,n.disabled=!0,clearInterval(e)}))}();
-//# sourceMappingURL=01-color-switcher.9cad8ed9.js.map
